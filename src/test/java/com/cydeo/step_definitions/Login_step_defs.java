a5947c21c1d18935865245fe5b6fa8531d288b41
AmirHesamB <amirh3sam.badiei@gmail.com>
2023-02-18 15:09:35 -0500

    update Login_step_defs.java

--- src/test/java/com/cydeo/step_definitions/Login_step_defs.java
+++ src/test/java/com/cydeo/step_definitions/Login_step_defs.java
@@ -3,33 +3,49 @@ package com.cydeo.step_definitions;
 import com.cydeo.pages.SeamlessLoginPage;
 import com.cydeo.utilities.ConfigurationReader;
 import com.cydeo.utilities.Driver;
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
+import io.cucumber.java.en.*;
 import org.junit.Assert;
 
 public class Login_step_defs {
-SeamlessLoginPage seamlessLoginPage =new SeamlessLoginPage();
-    @Given("the user is on the login page")
-    public void the_user_is_on_the_login_page() {
+
+    SeamlessLoginPage loginPage=new SeamlessLoginPage();
+    @Given("the user is on the log in page")
+    public void the_user_is_on_the_log_in_page() {
         Driver.getDriver().get(ConfigurationReader.getProperty("seamless.url"));
     }
-    @When("the user enter correct username")
-    public void the_user_enter_correct_username() {
-        seamlessLoginPage.txt_username.sendKeys(ConfigurationReader.getProperty("seamless.username"));
+    @When("the user enter valid username")
+    public void the_user_enter_valid_username() {
+        loginPage.txt_username.sendKeys(ConfigurationReader.getProperty("seamless.username"));
     }
     @When("the user enter valid password")
     public void the_user_enter_valid_password() {
-        seamlessLoginPage.txt_password.sendKeys(ConfigurationReader.getProperty("seamless.password"));
+        loginPage.txt_password.sendKeys(ConfigurationReader.getProperty("seamless.password"));
     }
-    @When("the user click the login button")
-    public void the_user_click_the_login_button() {
-   seamlessLoginPage.button_login.click();
+    @When("the user clicks the log in button")
+    public void the_user_clicks_the_log_in_button() {
+        loginPage.btn_log_in.click();
     }
-    @Then("the user should be able to login")
-    public void the_user_should_be_able_to_login() {
+    @Then("the user should be able to log in")
+    public void the_user_should_be_able_to_log_in() {
         Assert.assertTrue(Driver.getDriver().getTitle().contains("Files"));
     }
+
+
+
+
+
+    @When("the user enters username {string}")
+    public void the_user_enter_username_with_param(String username) {
+        loginPage.txt_username.sendKeys(ConfigurationReader.getProperty("seamless.username"));
+    }
+    @When("the user enters password {string}")
+    public void the_user_enter_password_with_param(String password) {
+        loginPage.txt_password.sendKeys(ConfigurationReader.getProperty("seamless.password"));
+    }
+
+
+
+
 
 
 }
