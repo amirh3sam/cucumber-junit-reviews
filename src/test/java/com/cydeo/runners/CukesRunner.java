11461231667f18c6a1470134d5ec215cfef87096
AmirHesamB <amirh3sam.badiei@gmail.com>
2023-02-18 12:33:32 -0500

    creat CukesRunner.java

---
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -0,0 +1,24 @@
+package com.cydeo.runners;
+
+
+
+import io.cucumber.junit.Cucumber;
+
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = "html:target/cucumber-reports.html",//type="html" of your report after : location of your report/name of your report.html
+        features = "src/test/resources/features", //same directors az project
+        glue = "com/cydeo/step_definitions",//under same directory
+        dryRun = false,//false mean run the test true means dont run just give me the methode
+        tags = "@scenarioOutline ",// give tag to library// empty tag="", means run everything
+        publish = true //generating a report with public link
+)
+public class CukesRunner {
+
+
+
+
+}
