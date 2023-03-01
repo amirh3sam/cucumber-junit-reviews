04729ac2389244c61c16856a3f57ddeebf595aab
Amirh3samB <69331074+amirh3sam@users.noreply.github.com>
2023-03-01 09:14:40 -0500

    Update Cukes Runner

--- src/test/java/com/cydeo/runners/CukesRunner.java
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -10,7 +10,8 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {"html:target/cucumber-reports.html",//type="html" of your report after : location of your report/name of your report.html
-        "json:target/cucumber.json","junit:target/junit/junit-report.xml"}
+        "json:target/cucumber.json",
+        "junit:target/junit/junit-report.xml"},
         features = "src/test/resources/features", //same directors az project
         glue = "com/cydeo/step_definitions",//under same directory
         dryRun = false,//false mean run the test true means dont run just give me the methode
