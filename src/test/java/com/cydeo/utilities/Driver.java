bdf975d52c6cf8a0e9e6fb938f4eef25905f6f90
Amirh3samB <69331074+amirh3sam@users.noreply.github.com>
2023-03-01 08:29:42 -0500

    Update Driver

--- src/test/java/com/cydeo/utilities/Driver.java
+++ src/test/java/com/cydeo/utilities/Driver.java
@@ -16,28 +16,28 @@ public class Driver {
     /*
     We make the Webdriver private, because we want to close access from outside the class We are making static, because we will use it in a static method
 
-     */
+     
     private static WebDriver driver;// default value = null
 
-    /*
+    
     Create resubale utility method which will return the same driver instance onece we call it.
     If an instance does not exits , it will creat first, and then it will always retuen same instance
-     */
+    
     public static WebDriver getDriver(){
 
         if(driver == null){
 
-                /*
+             
                 We will read our broswerType from configurration.properties file.
                 This way , we canotrol which broswer is open from outside our code
 
-                 */
+               
             String browserType = ConfigurationReader.getProperty("browser");
 
-                /*
+              
                 Depending on the browserType returned from the configuration.properties
                 switch statement will determine the "case" , and open the matching browser
-                 */
+             
             switch(browserType){
                 case"chrome":
                   //  WebDriverManager.chromedriver().setup();
@@ -59,22 +59,112 @@ public class Driver {
     }
 
 
-/*
+
 creat a new Driver.closeDriver(); it will use quit() method to quit browser
- */
+
 
     public static void closeDriver(){
         if(driver !=null){
-            /*
+         
             this line terminate the currently existing driver completley. It will not exist going forward
-             */
+          
             driver.quit();
-            /*
+           
             We assign the value back to "null" so that my "singleton" can creat a newer one if needed.
-             */
+        
+            driver = null;
+        }
+      }
+   */
+   
+    private static WebDriver driver;
+
+    public static WebDriver getDriver() {
+        if (driver == null) {
+            if (System.getProperty("BROWSER") == null) {
+                browser = ConfigurationReader.getProperty("browser");
+            } else {
+                browser = System.getProperty("BROWSER");
+            }
+            System.out.println("Browser: " + browser);
+            switch (browser) {
+                case "remote-chrome":
+                    try {
+                        // assign your grid server address
+                        String gridAddress = "localhost";
+                        URL url = new URL("http://" + gridAddress + ":4444/ui");
+                        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+                        desiredCapabilities.setBrowserName("chrome");
+                        driver = new RemoteWebDriver(url, desiredCapabilities);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                case "remote-firefox":
+                    try {
+                        // assign your grid server address
+                        String gridAddress = "174.129.57.20";
+                        URL url = new URL("http://" + gridAddress + ":4444/wd/hub");
+                        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+                        desiredCapabilities.setBrowserName("firefox");
+                        driver = new RemoteWebDriver(url, desiredCapabilities);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                    break;
+                case "chrome-headless":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver(new ChromeOptions().setHeadless(true));
+                    break;
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                    break;
+                case "firefox-headless":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver(new FirefoxOptions().setHeadless(true));
+                    break;
+
+                case "ie":
+                    if (System.getProperty("os.name").toLowerCase().contains("mac")) {
+                        throw new WebDriverException("Your operating system does not support the requested browser");
+                    }
+                    WebDriverManager.iedriver().setup();
+                    driver = new InternetExplorerDriver();
+                    break;
+
+                case "edge":
+                    if (System.getProperty("os.name").toLowerCase().contains("mac")) {
+                        throw new WebDriverException("Your operating system does not support the requested browser");
+                    }
+                    WebDriverManager.edgedriver().setup();
+                    driver = new EdgeDriver();
+                    break;
+
+                case "safari":
+                    if (System.getProperty("os.name").toLowerCase().contains("windows")) {
+                        throw new WebDriverException("Your operating system does not support the requested browser");
+                    }
+                    WebDriverManager.getInstance(SafariDriver.class).setup();
+                    driver = new SafariDriver();
+                    break;
+            }
+        }
+
+        return driver;
+    }
+
+    public static void closeDriver() {
+        if (driver != null) {
+            driver.quit();
             driver = null;
         }
     }
+
 
 }
 
